0f0de471b61b4a97a14dc29f0ff4999d71e10265
OlenkaTumak <olenatumak52@gmail.com>
2025-10-04 23:22:06 +0300

    small change in game manager

--- src/main/java/model/GameManager.java
+++ src/main/java/model/GameManager.java
@@ -1,11 +1,15 @@
 package model;
 
 public class GameManager {
+    private static CharacterFactory factory = new CharacterFactory();
+
     public void fight(Character c1, Character c2) {
         System.out.println("\nA fight begins between " + c1.getClass().getSimpleName() + " and "
                 + c2.getClass().getSimpleName() + "!");
-
-        while (c1.isAlive() && c2.isAlive()) {
+        int maxTurns = 10000;
+        int turn = 0;
+        while (c1.isAlive() && c2.isAlive() && turn < maxTurns) {
+            turn++;
             System.out.println(c1.getClass().getSimpleName() + " attacks!");
             c1.kick(c2);
             System.out.println(c2.getClass().getSimpleName() + " HP: " + c2.getHp());
@@ -24,5 +28,15 @@ public class GameManager {
                 break;
             }
         }
+        if (turn == maxTurns)
+            System.out.println("Fight ended in a draw!");
     }
+
+    public static void main(String[] args) {
+        GameManager manager = new GameManager();
+        Character c1 = factory.createRandomCharacter();
+        Character c2 = factory.createRandomCharacter();
+        manager.fight(c1, c2);
+    }
+
 }
